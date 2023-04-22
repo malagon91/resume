3f9974673bf64d3831ae309ae36bfa4b650fac42
Miguel Malagon <malagonmiguel17@gmail.com>
2023-04-21 23:17:50 -0600

    add initial data

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,9 +1,16 @@
 import react from 'react'
+import Image from 'next/image'
 import styles from './Home.module.scss'
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
+      <header>
+        <Image src="/logo.png" alt="Miguel Malagón" width={100} height={100} />
+        <div>
+          <p className={styles.name}>Miguel Malagón</p>
+          <p className={styles.title}>Software engineer</p>
+        </div>
+      </header>
     </div>
   )
 }
